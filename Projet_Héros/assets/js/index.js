c9908ca5e86d10f337c19ee24305b5196eb739b2
Arnaud-Dlt <arnauddeletre@outlook.fr>
2023-01-17 09:45:21 +0000

    11H

--- Projet_Héros/assets/js/index.js
+++ Projet_Héros/assets/js/index.js
@@ -2,6 +2,10 @@ import { Enquete } from "./classes/enquete.js";
 import { EnqueteTest } from "./classes/test.js";
 
 window.addEventListener("DOMContentLoaded", function() {
-    let choix=document.querySelectorAll("ul li")
+    let choix=document.querySelectorAll("ul li");
+    
+    for(let i=0;i<choix.length;i++){
+        
+    }
     
 });
